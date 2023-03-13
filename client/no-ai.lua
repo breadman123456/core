af776de9deeb2c7d933771230dd5236997a40a45
breadman <57912521+breadman123456@users.noreply.github.com>
2023-03-13 09:10:28 -0700

    Update no-ai.lua

--- client/no-ai.lua
+++ client/no-ai.lua
@@ -1,8 +1,8 @@
 -- Credits to Martin North
 -- Original Source: https://forum.cfx.re/t/release-remove-all-ai-and-remove-random-parked-vehicles/638724/1
 Citizen.CreateThread(function()
-    while true do
-    	SetVehicleDensityMultiplierThisFrame(0.0)
+	while true do
+		SetVehicleDensityMultiplierThisFrame(0.0)
 		SetPedDensityMultiplierThisFrame(0.0)
 		SetRandomVehicleDensityMultiplierThisFrame(0.0)
 		SetParkedVehicleDensityMultiplierThisFrame(0.0)
