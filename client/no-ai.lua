3299c343998afa732e715feb599d2c1220016de4
breadman <57912521+breadman123456@users.noreply.github.com>
2023-03-13 09:07:35 -0700

    Update no-ai.lua

--- client/no-ai.lua
+++ client/no-ai.lua
@@ -1,30 +1,19 @@
--- Credits to Loqrin
--- Original Source: https://github.com/Loqrin/FiveM-Gamemode-Wreckfest/blob/master/modules/no_ai.lua
-local function removeAI(playerPed, playerPos)
-    SetVehicleDensityMultiplierThisFrame(0.0)
-    SetPedDensityMultiplierThisFrame(0.0)
-    SetRandomVehicleDensityMultiplierThisFrame(0.0)
-    SetParkedVehicleDensityMultiplierThisFrame(0.0)
-    SetScenarioPedDensityMultiplierThisFrame(0.0, 0.0)
-
-    RemoveVehiclesFromGeneratorsInArea(playerPos.x - 500.0, playerPos.y - 500.0, playerPos.z - 500.0, playerPos.x + 500.0, playerPos.y + 500.0, playerPos.z + 500.0);
-    ClearAreaOfCops(playerPos.x, playerPos.y, playerPos.z, 400.0)
-
-    SetGarbageTrucks(0)
-    SetRandomBoats(0)
-    
-    for i = 1, 15 do
-        EnableDispatchService(i, false)
-    end
-end
-
+-- Credits to Martin North
+-- Original Source: https://forum.cfx.re/t/release-remove-all-ai-and-remove-random-parked-vehicles/638724/1
 Citizen.CreateThread(function()
-    if noAIModule then
-        while true do
-            local playerPed = GetPlayerPed(-1)
-            local playerPos = GetEntityCoords(playerPed)
-            removeAI(playerPed, playerPos)
-            Citizen.Wait(1)
-        end
-    end
+    while true do
+    	SetVehicleDensityMultiplierThisFrame(0.0)
+		SetPedDensityMultiplierThisFrame(0.0)
+		SetRandomVehicleDensityMultiplierThisFrame(0.0)
+		SetParkedVehicleDensityMultiplierThisFrame(0.0)
+		SetScenarioPedDensityMultiplierThisFrame(0.0, 0.0)
+		
+		local playerPed = GetPlayerPed(-1)
+		local pos = GetEntityCoords(playerPed) 
+		RemoveVehiclesFromGeneratorsInArea(pos['x'] - 500.0, pos['y'] - 500.0, pos['z'] - 500.0, pos['x'] + 500.0, pos['y'] + 500.0, pos['z'] + 500.0);
+		
+		SetGarbageTrucks(0)
+		SetRandomBoats(0)    	
+		Citizen.Wait(1)
+	end
 end)
