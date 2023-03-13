d5bde9a9efeb22206fb6f37fb7d9081c575d3bd5
breadman <57912521+breadman123456@users.noreply.github.com>
2023-03-13 08:39:50 -0700

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version "cerulean"
+games { "gta5" }
+
+author "LMMG"
+description "FiveM core containing all sorts of essentials."
+version "0.0.1"
+
+client_scripts {
+}
+
+server_scripts {
+}
